23ecf0a1f92a808c25ec670618558c9b7fe782cb
Alex Gil <alejandro.gilh@estudiante.uam.es>
2016-08-10 20:54:19 +0200

    Adding movielens datasett (100k)
    Dataset and imports

--- src/main/java/userBased/LensKitTestUB.java
+++ src/main/java/userBased/LensKitTestUB.java
@@ -1,20 +1,22 @@
 package userBased;
 import java.io.File;
 import java.util.List;
-import org.grouplens.lenskit.*;
+
+import org.grouplens.lenskit.ItemRecommender;
+import org.grouplens.lenskit.ItemScorer;
+import org.grouplens.lenskit.RecommenderBuildException;
 import org.grouplens.lenskit.baseline.BaselineScorer;
 import org.grouplens.lenskit.baseline.ItemMeanRatingItemScorer;
 import org.grouplens.lenskit.baseline.UserMeanBaseline;
 import org.grouplens.lenskit.baseline.UserMeanItemScorer;
-import org.grouplens.lenskit.core.*;
+import org.grouplens.lenskit.core.LenskitConfiguration;
+import org.grouplens.lenskit.core.LenskitRecommender;
 import org.grouplens.lenskit.data.dao.EventDAO;
 import org.grouplens.lenskit.data.text.TextEventDAO;
 import org.grouplens.lenskit.knn.user.UserUserItemScorer;
 import org.grouplens.lenskit.scored.ScoredId;
 import org.grouplens.lenskit.transform.normalize.DefaultUserVectorNormalizer;
-import org.grouplens.lenskit.transform.normalize.MeanCenteringVectorNormalizer;
 import org.grouplens.lenskit.transform.normalize.UserVectorNormalizer;
-import org.grouplens.lenskit.transform.normalize.VectorNormalizer;
 
 public class LensKitTestUB {
 
