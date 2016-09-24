59a5ba4ba71b4213ad8e67c177cd6206bee78c14
Alex Gil <alejandro.gilh@estudiante.uam.es>
2016-09-24 18:41:19 +0200

    Fixing ThresholdSimilarity

--- src/main/java/myRecommender/ThresholdSimilarity.java
+++ src/main/java/myRecommender/ThresholdSimilarity.java
@@ -2,32 +2,30 @@ package myRecommender;
 
 import java.util.function.IntToDoubleFunction;
 import java.util.stream.Stream;
-
 import org.ranksys.core.util.tuples.Tuple2id;
 
+import es.uam.eps.ir.ranksys.fast.preference.FastPreferenceData;
+import es.uam.eps.ir.ranksys.fast.preference.IdxPref;
 import es.uam.eps.ir.ranksys.nn.sim.Similarity;
+import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
+import it.unimi.dsi.fastutil.ints.IntSet;
 
 public class ThresholdSimilarity implements Similarity{
 	
 	private double lowerThreshold;
 	private double higherThreshold;
 	protected final Similarity sim;
+	private FastPreferenceData<?, ?> data;
 	
-	public ThresholdSimilarity(Similarity sim, double lowerThreshold, double higherThreshold) {
+	public ThresholdSimilarity(FastPreferenceData<?, ?> data, Similarity sim, double lowerThreshold, double higherThreshold) {
 
+		this.data = data;
 		this.sim = sim;
 		this.lowerThreshold = lowerThreshold;
 		this.higherThreshold = higherThreshold;
 	}
 	
-	@Override
-    public double similarity(int idx1, int idx2) {
-        if (sim.similarity(idx1, idx2) > lowerThreshold && sim.similarity(idx1, idx2) < higherThreshold)
-        	return sim.similarity(idx1).applyAsDouble(idx2);
-        
-        return 0.0;
-    }
-
+	
 	@Override
 	public Stream<Tuple2id> similarElems(int idx) {
 		return sim.similarElems(idx);
@@ -35,8 +33,25 @@ public class ThresholdSimilarity implements Similarity{
 
 	@Override
 	public IntToDoubleFunction similarity(int idx) {
-		return sim.similarity(idx);		//No se que devolver
+		
+		IntSet set = new IntOpenHashSet();
+        data.getUidxPreferences(idx).map(IdxPref::v1).forEach(set::add);
+
+        return idx2 -> {
+            int product = (int) data.getUidxPreferences(idx2)
+                    .map(IdxPref::v1)
+                    .filter(set::contains)
+                    .count();
+
+            if (checkThresHold(product, lowerThreshold, higherThreshold))
+            	return product;
+            return 0.0;
+        };
 	}
 	
-
+	private boolean checkThresHold(double sim, double lowerThreshold, double higherThreshold){
+		
+		return (sim > lowerThreshold && sim < higherThreshold);
+	}
+	
 }
