41b329d1ae65ab80751775b266d40f64f395f763
Alejandro Gil <alejandrogil@MacBook-Pro-de-Alejandro.local>
2016-10-04 14:16:23 +0200

    Pearson v1

--- src/main/java/myRecommender/PearsonCorrelationSimilarity.java
+++ src/main/java/myRecommender/PearsonCorrelationSimilarity.java
@@ -1,21 +1,23 @@
 package myRecommender;
 
+import static java.util.stream.IntStream.range;
+import static org.ranksys.core.util.tuples.Tuples.tuple;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.IntToDoubleFunction;
 import java.util.stream.Stream;
 
-import org.apache.commons.math3.analysis.function.Pow;
 import org.ranksys.core.util.tuples.Tuple2id;
 
 import es.uam.eps.ir.ranksys.core.util.Stats;
 import es.uam.eps.ir.ranksys.fast.preference.FastPreferenceData;
 import es.uam.eps.ir.ranksys.fast.preference.IdxPref;
 import es.uam.eps.ir.ranksys.nn.sim.Similarity;
-import es.uam.eps.ir.ranksys.nn.sim.VectorSimilarity;
 import it.unimi.dsi.fastutil.doubles.DoubleIterator;
 import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
 import it.unimi.dsi.fastutil.ints.Int2DoubleOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntIterator;
 
 public class PearsonCorrelationSimilarity implements Similarity {
 
@@ -94,15 +96,53 @@ public class PearsonCorrelationSimilarity implements Similarity {
         };
 	}
 
-	private double sim(double product, double norm2A, double norm2B){
-		
+	private double sim(double product, double norm2A, double norm2B){	
 		return product / Math.sqrt(norm2A * norm2B);
 	}
 
 	@Override
-	public Stream<Tuple2id> similarElems(int idx) {
-		// TODO Auto-generated method stub
-		return null;
+	public Stream<Tuple2id> similarElems(int idx1) {
+
+        if (data.useIteratorsPreferentially()) {
+            if (dense) {
+                double n2a = norm2Array[idx1];
+
+                double[] productMap = getFasterProductArray(idx1);
+                return range(0, productMap.length)
+                        .filter(i -> productMap[i] != 0.0)
+                        .mapToObj(i -> tuple(i, sim(productMap[i], n2a, norm2Array[i])));
+            } else {
+                double n2a = norm2Map.get(idx1);
+
+                return getFasterProductMap(idx1).int2DoubleEntrySet().stream()
+                        .map(e -> {
+                            int idx2 = e.getIntKey();
+                            double coo = e.getDoubleValue();
+                            double n2b = norm2Map.get(idx2);
+                            return tuple(idx2, sim(coo, n2a, n2b));
+                        });
+            }
+        } else {
+            if (dense) {
+                double n2a = norm2Array[idx1];
+
+                double[] productMap = getProductArray(idx1);
+                return range(0, productMap.length)
+                        .filter(i -> productMap[i] != 0.0)
+                        .mapToObj(i -> tuple(i, sim(productMap[i], n2a, norm2Array[i])));
+            } else {
+                double n2a = norm2Map.get(idx1);
+
+                return getProductMap(idx1).int2DoubleEntrySet().stream()
+                        .map(e -> {
+                            int idx2 = e.getIntKey();
+                            double coo = e.getDoubleValue();
+                            double n2b = norm2Map.get(idx2);
+                            return tuple(idx2, sim(coo, n2a, n2b));
+                        });
+            }
+        }
+
 	}
 
 	
@@ -121,5 +161,75 @@ public class PearsonCorrelationSimilarity implements Similarity {
                 .mapToDouble(IdxPref::v2)
                 .map(x -> Math.pow(x - stats.get(idx).getMean(), 2))
                 .sum();
+    }
+	
+	private Int2DoubleMap getProductMap(int idx1) {
+        Int2DoubleOpenHashMap productMap = new Int2DoubleOpenHashMap();
+        productMap.defaultReturnValue(0.0);
+
+        data.getUidxPreferences(idx1).forEach(ip -> {
+            data.getIidxPreferences(ip.v1).forEach(up -> {
+                productMap.addTo(up.v1, ip.v2 * up.v2);
+            });
+        });
+
+        productMap.remove(idx1);
+
+        return productMap;
+    }
+
+    private double[] getProductArray(int idx1) {
+        double[] productMap = new double[data.numUsers()];
+
+        data.getUidxPreferences(idx1).forEach(ip -> {
+            data.getIidxPreferences(ip.v1).forEach(up -> {
+                productMap[up.v1] += ip.v2 * up.v2;
+            });
+        });
+
+        productMap[idx1] = 0.0;
+
+        return productMap;
+    }
+
+	private Int2DoubleMap getFasterProductMap(int uidx) {
+        Int2DoubleOpenHashMap productMap = new Int2DoubleOpenHashMap();
+        productMap.defaultReturnValue(0.0);
+
+        IntIterator iidxs = data.getUidxIidxs(uidx);
+        DoubleIterator ivs = data.getUidxVs(uidx);
+        while (iidxs.hasNext()) {
+            int iidx = iidxs.nextInt();
+            double iv = ivs.nextDouble();
+            IntIterator vidxs = data.getIidxUidxs(iidx);
+            DoubleIterator vvs = data.getIidxVs(iidx);
+            while (vidxs.hasNext()) {
+                productMap.addTo(vidxs.nextInt(), iv * vvs.nextDouble());
+            }
+        }
+
+        productMap.remove(uidx);
+
+        return productMap;
+    }
+
+    private double[] getFasterProductArray(int uidx) {
+        double[] productMap = new double[data.numUsers()];
+
+        IntIterator iidxs = data.getUidxIidxs(uidx);
+        DoubleIterator ivs = data.getUidxVs(uidx);
+        while (iidxs.hasNext()) {
+            int iidx = iidxs.nextInt();
+            double iv = ivs.nextDouble();
+            IntIterator vidxs = data.getIidxUidxs(iidx);
+            DoubleIterator vvs = data.getIidxVs(iidx);
+            while (vidxs.hasNext()) {
+                productMap[vidxs.nextInt()] += iv * vvs.nextDouble();
+            }
+        }
+
+        productMap[uidx] = 0.0;
+
+        return productMap;
     }
 }
