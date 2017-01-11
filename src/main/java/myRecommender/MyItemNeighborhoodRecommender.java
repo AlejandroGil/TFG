331a9036e61a52e6eb97e3950511752f5219e70b
Alejandro <alejandro.bellogin@uam.es>
2017-01-11 23:56:51 +0100

    Fixed bug in recommenders (it happened when the similarity returned a NaN or Infinity). Also: fixed a problem with normalization.

--- src/main/java/myRecommender/MyItemNeighborhoodRecommender.java
+++ src/main/java/myRecommender/MyItemNeighborhoodRecommender.java
@@ -13,163 +13,177 @@ import es.uam.eps.ir.ranksys.rec.fast.FastRankingRecommender;
 import it.unimi.dsi.fastutil.ints.Int2DoubleMap;
 import it.unimi.dsi.fastutil.ints.Int2DoubleOpenHashMap;
 import myRecommender.MyUserNeighborhoodRecommender.TRANSFORM;
+
 /**
  * Item-based nearest neighbors recommender.
  *
  * F. Aiolli. Efficient Top-N Recommendation for Very Large Scale Binary Rated
  * Datasets. RecSys 2013.
  *
- * Paolo Cremonesi, Yehuda Koren, and Roberto Turrin. Performance of
- * recommender algorithms on top-n recommendation tasks. RecSys 2010.
+ * Paolo Cremonesi, Yehuda Koren, and Roberto Turrin. Performance of recommender
+ * algorithms on top-n recommendation tasks. RecSys 2010.
  *
  * C. Desrosiers, G. Karypis. A comprehensive survey of neighborhood-based
  * recommendation methods. Recommender Systems Handbook.
  *
  * @author Alejandro Gil
  *
- * @param <U> type of the users
- * @param <I> type of the items
+ * @param <U>
+ *            type of the users
+ * @param <I>
+ *            type of the items
  */
 public class MyItemNeighborhoodRecommender<U, I> extends FastRankingRecommender<U, I> {
 
 	protected TRANSFORM t;
-    /**
-     * Preference data.
-     */
-    protected final FastPreferenceData<U, I> data;
+	/**
+	 * Preference data.
+	 */
+	protected final FastPreferenceData<U, I> data;
 
-    /**
-     * User neighborhood.
-     */
-    protected final ItemNeighborhood<U> neighborhood;
+	/**
+	 * User neighborhood.
+	 */
+	protected final ItemNeighborhood<U> neighborhood;
 
+	protected final ItemSimilarity<U> similarity;
 
-    protected final ItemSimilarity<U> similarity;
+	/**
+	 * Exponent of the similarity.
+	 */
+	protected final int q;
 
-    /**
-     * Exponent of the similarity.
-     */
-    protected final int q;
+	/*
+	 * Map containing the ratings of the users to calculate means and deviations
+	 */
+	protected Map<Integer, Stats> stats;
+	protected boolean normalize;
 
-    /*Map containing the ratings of the users to calculate means and deviations*/
-    protected Map<Integer, Stats> stats;
-    protected boolean normalize;
+	/**
+	 * Constructor.
+	 *
+	 * @param data
+	 *            preference data
+	 * @param neighborhood
+	 *            user neighborhood
+	 * @param q
+	 *            exponent of the similarity
+	 */
+	public MyItemNeighborhoodRecommender(FastPreferenceData<U, I> data, ItemNeighborhood<U> neighborhood, int q,
+			ItemSimilarity<U> sim, TRANSFORM std, boolean normalize) {
+		super(data, data);
+		this.data = data;
+		this.neighborhood = neighborhood;
+		this.q = q;
 
+		this.similarity = sim;
+		this.normalize = normalize;
+		this.t = std;
 
-    /**
-     * Constructor.
-     *
-     * @param data preference data
-     * @param neighborhood user neighborhood
-     * @param q exponent of the similarity
-     */
-    public MyItemNeighborhoodRecommender(FastPreferenceData<U, I> data, ItemNeighborhood<U> neighborhood, int q, ItemSimilarity<U> sim, TRANSFORM std, boolean normalize) {
-        super(data, data);
-        this.data = data;
-        this.neighborhood = neighborhood;
-        this.q = q;
+		stats = new HashMap<>();
+		data.getAllIidx().forEach(iIndex -> {
+			Stats s = new Stats();
+			stats.put(iIndex, s);
+			data.getIidxPreferences(iIndex).forEach(p -> {
+				s.accept(p.v2);
+			});
+		});
 
-        this.similarity = sim;
-        this.normalize = normalize;
-        this.t = std;
+	}
 
-        stats = new HashMap<>();
-        data.getAllIidx().forEach(iIndex -> {
-            Stats s = new  Stats();
-            stats.put(iIndex, s);
-            data.getIidxPreferences(iIndex).forEach(p -> {
-                s.accept(p.v2);
-            });
-        });
+	/**
+	 * Returns a map of item-score pairs.
+	 *
+	 * @param uidx
+	 *            index of the user whose scores are predicted
+	 * @return a map of item-score pairs
+	 */
+	@Override
+	public Int2DoubleMap getScoresMap(int uidx) {
+		Int2DoubleOpenHashMap cMap = new Int2DoubleOpenHashMap();
+		cMap.defaultReturnValue(0.0);
+		Int2DoubleOpenHashMap scoresMap = new Int2DoubleOpenHashMap();
+		scoresMap.defaultReturnValue(0.0);
+		Int2DoubleOpenHashMap count = new Int2DoubleOpenHashMap();
+		count.defaultReturnValue(0.0);
 
-    }
+		data.getUidxPreferences(uidx).forEach(jp -> {
+			int i = jp.v1;
 
-    /**
-     * Returns a map of item-score pairs.
-     *
-     * @param uidx index of the user whose scores are predicted
-     * @return a map of item-score pairs
-     */
-    @Override
-    public Int2DoubleMap getScoresMap(int uidx) {
-        Int2DoubleOpenHashMap cMap = new Int2DoubleOpenHashMap();
-        cMap.defaultReturnValue(0.0);
-        Int2DoubleOpenHashMap scoresMap = new Int2DoubleOpenHashMap();
-        scoresMap.defaultReturnValue(0.0);
-        Int2DoubleOpenHashMap count = new Int2DoubleOpenHashMap();
-        count.defaultReturnValue(0.0);
+			neighborhood.getNeighbors(i).forEach(is -> {
+				int j = is.v1;
+				// System.out.println("Asking similarity for items " + i + " and
+				// " + j + " -> " + data.iidx2item(i) + " and " +
+				// data.iidx2item(j));
+				double sim = similarity.similarity(i, j);
+				// System.out.println("Similarity found: " + sim);
 
-        data.getUidxPreferences(uidx).forEach(jp -> {
-        	int i = jp.v1;
+				double w = pow(sim, q);
 
-        	neighborhood.getNeighbors(i).forEach(is -> {
-            	int j = is.v1;
-            	//System.out.println("Asking similarity for items " + i + " and " + j + " -> " + data.iidx2item(i) + " and " + data.iidx2item(j));
-            	double sim = similarity.similarity(i, j);
-            	//System.out.println("Similarity found: " + sim);
+				double t3 = 0.0;
+				switch (t) {
+				case STD:
+					t3 = jp.v2;
+					break;
+				case MC:
+					t3 = jp.v2 - stats.get(j).getMean();
+					break;
+				case Z:
+					t3 = (jp.v2 - stats.get(j).getMean()) / stats.get(j).getStandardDeviation();
+					break;
 
-                double w = pow(sim, q);
-                cMap.addTo(1, Math.abs(w));
+				default:
+					break;
+				}
+				double p = w * t3;
+				if (Double.isFinite(p)) {
+					cMap.addTo(j, Math.abs(w));
+					scoresMap.addTo(j, p);
+					count.addTo(j, 1);
+				}
+			});
+		});
 
-            	double t3 = 0.0;
-            	switch (t) {
-    			case STD:
-    				t3 = jp.v2;
-    				break;
-    			case MC:
-    				t3 = jp.v2 - stats.get(j).getMean();
-    				break;
-    			case Z:
-    				t3 = (jp.v2 - stats.get(j).getMean())/stats.get(j).getStandardDeviation();
-    				break;
+		Int2DoubleOpenHashMap scoresMap2 = new Int2DoubleOpenHashMap();
+		scoresMap2.defaultReturnValue(0.0);
+		scoresMap.forEach((i, v) -> {
 
-    			default:
-    				break;
-    			}
-                double p = w * t3;
-                scoresMap.addTo(j, p);
-                count.addTo(j, 1);
-            });
-        });
+			double t1 = 0.0;
+			switch (t) {
+			case STD:
+				break;
+			case MC:
+			case Z:
+				t1 = stats.get(i).getMean();
+				break;
 
-        Int2DoubleOpenHashMap scoresMap2 = new Int2DoubleOpenHashMap();
-        scoresMap2.defaultReturnValue(0.0);
-        scoresMap.forEach((i,v) -> {
+			default:
+				t1 = 0.0;
+				break;
+			}
 
-            double t1 = 0.0;
-            switch (t) {
-    		case STD:
-    			break;
-    		case MC:
-    		case Z:
-    			t1 = stats.get(i).getMean();
-    			break;
+			double t2 = 1.0;
+			switch (t) {
+			case STD:
+			case MC:
+				t2 = 1.0;
+				break;
+			case Z:
+				t2 = stats.get(i).getStandardDeviation();
+				break;
 
-    		default:
-    			t1 = 0.0;
-    			break;
-    		}
+			default:
+				break;
+			}
 
-            double t2 = 1.0;
-        	switch (t) {
-    		case STD:
-    		case MC:
-    			t2 = 1.0;
-    			break;
-    		case Z:
-    			t2 = stats.get(i).getStandardDeviation();
-    			break;
+			final double b = normalize ? t2 / cMap.get(i) : t2;
 
-    		default:
-    			break;
-    		}
+			double s = t1 + b * v;
+			if (Double.isFinite(s)) {
+				scoresMap2.addTo(i, s);
+			}
+		});
 
-            final double b = normalize ? t2 / cMap.get(1) : t2;
-
-        	double s = t1 + b * v;
-        	scoresMap2.addTo(i, s);
-        });
-
-        return scoresMap2;
-    }
+		return scoresMap2;
+	}
 }
--- src/main/java/myRecommender/MyUserNeighborhoodRecommender.java
+++ src/main/java/myRecommender/MyUserNeighborhoodRecommender.java
@@ -10,103 +10,112 @@ import it.unimi.dsi.fastutil.ints.Int2DoubleOpenHashMap;
 import static java.lang.Math.pow;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * User-based nearest neighbors recommender.
  * 
  * F. Aiolli. Efficient Top-N Recommendation for Very Large Scale Binary Rated
  * Datasets. RecSys 2013.
  * 
- * Paolo Cremonesi, Yehuda Koren, and Roberto Turrin. Performance of 
- * recommender algorithms on top-n recommendation tasks. RecSys 2010.
+ * Paolo Cremonesi, Yehuda Koren, and Roberto Turrin. Performance of recommender
+ * algorithms on top-n recommendation tasks. RecSys 2010.
  * 
- * C. Desrosiers, G. Karypis. A comprehensive survey of neighborhood-based 
+ * C. Desrosiers, G. Karypis. A comprehensive survey of neighborhood-based
  * recommendation methods. Recommender Systems Handbook.
  *
  * @author Alejandro Gil
  *
- * @param <U> type of the users
- * @param <I> type of the items
+ * @param <U>
+ *            type of the users
+ * @param <I>
+ *            type of the items
  */
 public class MyUserNeighborhoodRecommender<U, I> extends FastRankingRecommender<U, I> {
-	
-	public static enum TRANSFORM{
-		STD,
-		MC,
-		Z
+
+	public static enum TRANSFORM {
+		STD, MC, Z
 	}
 
 	protected TRANSFORM t;
-    /**
-     * Preference data.
-     */
-    protected final FastPreferenceData<U, I> data;
+	/**
+	 * Preference data.
+	 */
+	protected final FastPreferenceData<U, I> data;
 
-    /**
-     * User neighborhood.
-     */
-    protected final UserNeighborhood<U> neighborhood;
-    
-    
-    protected final UserSimilarity<U> similarity;
+	/**
+	 * User neighborhood.
+	 */
+	protected final UserNeighborhood<U> neighborhood;
 
-    /**
-     * Exponent of the similarity.
-     */
-    protected final int q;
-    
-    /*Map containing the ratings of the users to calculate means and deviations*/
-    protected Map<Integer, Stats> stats;
-    protected boolean normalize;
+	protected final UserSimilarity<U> similarity;
 
+	/**
+	 * Exponent of the similarity.
+	 */
+	protected final int q;
 
-    /**
-     * Constructor.
-     *
-     * @param data preference data
-     * @param neighborhood user neighborhood
-     * @param q exponent of the similarity
-     * @param sim similarity of users
-     * @param tr type of transformation (standard, mean centeringo or z-score)
-     * @param normalize choose whether the ratings are normalized or not
-     */
-    public MyUserNeighborhoodRecommender(FastPreferenceData<U, I> data, UserNeighborhood<U> neighborhood, int q, UserSimilarity<U> sim, TRANSFORM tr, boolean normalize) {
-        super(data, data);
-        this.data = data;
-        this.neighborhood = neighborhood;
-        this.q = q;
-        
-        this.similarity = sim;
-        this.normalize = normalize;
-        this.t = tr;
-        
-        stats = new HashMap<>();
-        data.getAllUidx().forEach(uIndex -> {
-            Stats s = new  Stats();
-            stats.put(uIndex, s);
-            data.getUidxPreferences(uIndex).forEach(p -> {
-                s.accept(p.v2);
-            });
-        });
-        
-    }
+	/*
+	 * Map containing the ratings of the users to calculate means and deviations
+	 */
+	protected Map<Integer, Stats> stats;
+	protected boolean normalize;
 
-    /**
-     * Returns a map of item-score pairs.
-     *
-     * @param uidx index of the user whose scores are predicted
-     * @return a map of item-score pairs
-     */
-    @Override
-    public Int2DoubleMap getScoresMap(int uidx) {
-        Int2DoubleOpenHashMap cMap = new Int2DoubleOpenHashMap();
-        cMap.defaultReturnValue(0.0);
-        Int2DoubleOpenHashMap scoresMap = new Int2DoubleOpenHashMap();
-        scoresMap.defaultReturnValue(0.0);
-        Int2DoubleOpenHashMap count = new Int2DoubleOpenHashMap();
-        count.defaultReturnValue(0.0);
+	/**
+	 * Constructor.
+	 *
+	 * @param data
+	 *            preference data
+	 * @param neighborhood
+	 *            user neighborhood
+	 * @param q
+	 *            exponent of the similarity
+	 * @param sim
+	 *            similarity of users
+	 * @param tr
+	 *            type of transformation (standard, mean centeringo or z-score)
+	 * @param normalize
+	 *            choose whether the ratings are normalized or not
+	 */
+	public MyUserNeighborhoodRecommender(FastPreferenceData<U, I> data, UserNeighborhood<U> neighborhood, int q,
+			UserSimilarity<U> sim, TRANSFORM tr, boolean normalize) {
+		super(data, data);
+		this.data = data;
+		this.neighborhood = neighborhood;
+		this.q = q;
 
-        double t1 = 0.0;
-    	switch (t) {
+		this.similarity = sim;
+		this.normalize = normalize;
+		this.t = tr;
+
+		stats = new HashMap<>();
+		data.getAllUidx().forEach(uIndex -> {
+			Stats s = new Stats();
+			stats.put(uIndex, s);
+			data.getUidxPreferences(uIndex).forEach(p -> {
+				s.accept(p.v2);
+			});
+		});
+
+	}
+
+	/**
+	 * Returns a map of item-score pairs.
+	 *
+	 * @param uidx
+	 *            index of the user whose scores are predicted
+	 * @return a map of item-score pairs
+	 */
+	@Override
+	public Int2DoubleMap getScoresMap(int uidx) {
+		Int2DoubleOpenHashMap cMap = new Int2DoubleOpenHashMap();
+		cMap.defaultReturnValue(0.0);
+		Int2DoubleOpenHashMap scoresMap = new Int2DoubleOpenHashMap();
+		scoresMap.defaultReturnValue(0.0);
+		Int2DoubleOpenHashMap count = new Int2DoubleOpenHashMap();
+		count.defaultReturnValue(0.0);
+
+		double t1 = 0.0;
+		switch (t) {
 		case STD:
 			break;
 		case MC:
@@ -118,9 +127,9 @@ public class MyUserNeighborhoodRecommender<U, I> extends FastRankingRecommender<
 			t1 = 0.0;
 			break;
 		}
-        
-        double t2 = 1.0;
-    	switch (t) {
+
+		double t2 = 1.0;
+		switch (t) {
 		case STD:
 		case MC:
 			t2 = 1.0;
@@ -132,52 +141,52 @@ public class MyUserNeighborhoodRecommender<U, I> extends FastRankingRecommender<
 		default:
 			break;
 		}
-        
-    	neighborhood.getNeighbors(uidx).forEach(vs -> {
 
-        	double sim = similarity.similarity(uidx, vs.v1);
-        	
-            double w = pow(sim, q);
-            /*sum of similarities (when normalizing)*/
-            cMap.addTo(1, Math.abs(w));
-            
-            data.getUidxPreferences(vs.v1).forEach(iv -> {
-            	
-            	double t3 = 0.0;
-            	switch (t) {
-    			case STD:
-    				t3 = iv.v2;
-    				break;
-    			case MC:
-    				t3 = iv.v2 - stats.get(vs.v1).getMean();
-    				break;
-    			case Z:
-    				t3 = (iv.v2 - stats.get(vs.v1).getMean())/stats.get(vs.v1).getStandardDeviation();
-    				break;
+		neighborhood.getNeighbors(uidx).forEach(vs -> {
 
-    			default:
-    				break;
-    			}
-                double p = w * t3;
-                scoresMap.addTo(iv.v1, p);
-                count.addTo(iv.v1, 1);
-            });
-        });
-       
-        final double b = normalize ? t2 / cMap.get(1) : t2;
-        final double a = t1;
+			double sim = similarity.similarity(uidx, vs.v1);
 
-        Int2DoubleOpenHashMap scoresMap2 = new Int2DoubleOpenHashMap();
-        scoresMap2.defaultReturnValue(0.0);
-        scoresMap.forEach((k,v) -> {
-        	double s = a + b * v;
-        	scoresMap2.addTo(k, s);
-        });
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(scoresMap);
-//        System.out.println(scoresMap2);
+			double w = pow(sim, q);
+			if (Double.isFinite(w)) {
+				data.getUidxPreferences(vs.v1).forEach(iv -> {
 
-        return scoresMap2;
-    }    
+					double t3 = 0.0;
+					switch (t) {
+					case STD:
+						t3 = iv.v2;
+						break;
+					case MC:
+						t3 = iv.v2 - stats.get(vs.v1).getMean();
+						break;
+					case Z:
+						t3 = (iv.v2 - stats.get(vs.v1).getMean()) / stats.get(vs.v1).getStandardDeviation();
+						break;
+
+					default:
+						break;
+					}
+					double p = w * t3;
+					if (Double.isFinite(p)) {
+						scoresMap.addTo(iv.v1, p);
+						count.addTo(iv.v1, 1);
+						/* sum of similarities (when normalizing) */
+						cMap.addTo(iv.v1, Math.abs(w));
+					}
+				});
+			}
+		});
+
+		final double a = t1;
+		final double b = t2;
+
+		Int2DoubleOpenHashMap scoresMap2 = new Int2DoubleOpenHashMap();
+		scoresMap2.defaultReturnValue(0.0);
+		scoresMap.forEach((k, v) -> {
+			double b2 = normalize ? b / cMap.get(k) : b;
+			double s = a + b2 * v;
+			scoresMap2.addTo(k, s);
+		});
+
+		return scoresMap2;
+	}
 }
